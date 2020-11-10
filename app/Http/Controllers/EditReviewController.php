51eb2471e76f25c3268c62bda6df3e333af74a37
ykliba <mediaboxing2@gmail.com>
2020-11-10 16:27:04 +0900

    edit/create

--- app/Http/Controllers/EditReviewController.php
+++ app/Http/Controllers/EditReviewController.php
@@ -24,60 +24,57 @@ class EditReviewController extends Controller
     }
 
     function update(Request $request, $id) {
-        $review = Review::find($id);
-		if(!$review){
-			return back()->withError("編集できません");
-        }
-        
-        $input = $request->only('title', 'artist', 'desc', 'link');
-		$uploadInput = $request->only("image");
-		
-		// バリデーション
-		$validator = Validator::make($input, [
-			'title' => 'required|string',
-			'artist' => 'required|string',
-			'desc' => 'required|string',
-			'link' => 'nullable|string',
-		]);
-		if ($validator->fails()) {
-			return redirect('/review/create')
-			  ->withErrors($validator)
-			  ->withInput();
-		}
-		// 画像バリデーション
-		$uploadValidator = Validator::make($uploadInput, [
-          'image' => 'file|image|mimes:jpeg,jpg,png'
-		]);
-		if ($uploadValidator->fails()) {
-			return redirect('/review/create')
-			  ->withErrors($validator)
-			  ->withInput();
-        }
-
-        // youtubeのurlからidを取得
-		$url = $input["link"];
-		$youtube_id = substr(strrchr($url, "="), 1);
-		
-		// 保存
-		$review->title = $input['title'];
-		$review->artist = $input['artist'];
-		$review->desc = $input["desc"];
-		$review->link = $youtube_id;
-		$review->user_id = \Auth::id();
-		$review->update();
-		
-		// 画像保存
-		$is_change_image = false;		
-		if(isset($uploadInput["image"])) {
-			$path = $uploadInput["image"]->store("img");
-			if($path){
-				$review->image = $path;
-				$is_change_image = true;
+      $review = Review::find($id);
+			if(!$review){
+				return back()->withError("編集できません");
 			}
-		}
-		if($is_change_image){
+        
+			$input = $request->only('title', 'artist', 'desc', 'link');
+			$uploadInput = $request->only("image");
+			
+			// バリデーション
+			$validator = Validator::make($input, [
+				'title' => 'required|string',
+				'artist' => 'required|string',
+				'desc' => 'required|string',
+				'link' => 'nullable|string',
+			]);
+			if ($validator->fails()) {
+				return redirect('/review/edit')
+					->withErrors($validator)
+					->withInput();
+			}
+			// 画像バリデーション
+			$uploadValidator = Validator::make($uploadInput, [
+						'image' => 'file|image|mimes:jpeg,jpg,png'
+			]);
+			if ($uploadValidator->fails()) {
+				return redirect('/review/edit')
+					->withErrors($validator)
+					->withInput();
+			}
+
+					// youtubeのurlからidを取得
+			$url = $input["link"];
+			$youtube_id = substr(strrchr($url, "="), 1);
+			
+			// 保存
+			$review->title = $input['title'];
+			$review->artist = $input['artist'];
+			$review->desc = $input["desc"];
+			$review->link = $youtube_id;
+			$review->user_id = \Auth::id();
 			$review->update();
-		}
-        return redirect('/');
+			
+			// 画像保存
+			if ($uploadInput->isEmpty()) {
+				return redirect('/');
+			} else {
+				$path = Storage::disk('s3')->putFile('/', $uploadInput['image'], 'public');
+				$review->image = Storage::disk('s3')->url($path);
+				$review->save();
+				return redirect('/');
+			}
+
     }
 }
