7185bf85b17d42e037597ac2ce6a06de8cc6a43d
ykliba <mediaboxing2@gmail.com>
2021-01-26 13:27:56 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <script src="/js/app.js"></script> 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/099df60df2.js" crossorigin="anonymous"></script>
   
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -24,9 +25,6 @@
     @else
       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @endif
-    
-    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    
 
 </head>
 <body class="app_body"> 
